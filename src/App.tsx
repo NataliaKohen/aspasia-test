a21e1597f4d88bbbefefbaf8583d900255b37564
Natalia <kohen.natalia@gmail.com>
2025-03-12 16:26:19 -0300

    project creation

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" w-full flex justify-center">
+      <h1 className=" text-5xl font-bold underline">Libros</h1>
+    </div>
+  );
+}
+
+export default App;

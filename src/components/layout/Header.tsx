e75108449dd6227af46ab80eb193adb62edbbb98
Natalia <kohen.natalia@gmail.com>
2025-03-17 22:04:36 -0300

    creation layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="  text-white  bg-indigo-400 h-16">
+    <header className="  text-white font-bold  bg-cyan-500 h-16">
       <nav className="flex items-center w-full  h-16">
         <Link to="/" className="px-10">
           <h4>Inicio</h4>
         </Link>
         <Link to="/favoritos" className="px-10">
-          <h4>Favs</h4>
+          <h4>Favoritos</h4>
         </Link>
       </nav>
     </header>

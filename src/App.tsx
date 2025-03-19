f9c9845d69dfa0aae665f3a657970bfcb1982b7f
Natalia <kohen.natalia@gmail.com>
2025-03-18 21:14:49 -0300

    add zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound';
-import { BookProvider } from './context/BookContext';
 import Detail from './pages/Detail';
 import Favorites from './pages/Favorites';
 import Layout from './components/layout/Layout';
-import { FavoritesProvider } from './context/FavoritesContext';
 
 function App() {
   return (
-    <BookProvider>
-      <FavoritesProvider>
-        <Router>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/books/:id" element={<Detail />} />
-              <Route path="/favoritos" element={<Favorites />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </FavoritesProvider>
-    </BookProvider>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/books/:id" element={<Detail />} />
+          <Route path="/favoritos" element={<Favorites />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
